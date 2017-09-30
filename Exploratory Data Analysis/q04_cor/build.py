7bd966f7f45e676c3cbb66590d6f7a79bf223535
Aditya Mishra <mishraaditya6991@gmail.com>
2017-09-30 19:38:42 +0530

    Update build.py

--- Exploratory Data Analysis/q04_cor/build.py
+++ Exploratory Data Analysis/q04_cor/build.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data = pd.read_csv('../data/house_prices_multivariate.csv')
+data = pd.read_csv('data/house_prices_multivariate.csv')
 
 def cor(data):
     plt.figure(figsize=(15, 10))

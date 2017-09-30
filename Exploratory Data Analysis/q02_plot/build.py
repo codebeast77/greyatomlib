0c484e1dfeb29d7378ad7ae7ec4b490cc8ed6abc
Aditya Mishra <mishraaditya6991@gmail.com>
2017-09-30 19:39:48 +0530

    Update build.py

--- Exploratory Data Analysis/q02_plot/build.py
+++ Exploratory Data Analysis/q02_plot/build.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data = pd.read_csv('../data/house_prices_multivariate.csv')
+data = pd.read_csv('data/house_prices_multivariate.csv')
 
 def plot(num_cols):
     facet = None

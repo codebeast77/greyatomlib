25e24d8be960b928296056ee41d187dd8c53fd78
Jay Trivedi <mail.jaytrivedi@gmail.com>
2017-10-02 00:28:21 +0530

    Update build.py

--- greyatomlib/numpy_advanced/q01_get_total_deliveries_players/build.py
+++ greyatomlib/numpy_advanced/q01_get_total_deliveries_players/build.py
@@ -1,19 +1,12 @@
 # Default imports
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(os.curdir), '..','..'))
-
 import numpy as np
 
-def read_ipl_data(path, dtype=np.float64):
-    return np.genfromtxt(path, dtype=dtype, skip_header=1, delimiter=",")
-
-ipl_matches_array = read_ipl_data("../data/ipl_matches_small.csv", '|S50')
-
-
+ipl_matches_array =np.genfromtxt("./data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
 # Your Solution
 def get_total_deliveries_played(batsman):
     batsman_array = ipl_matches_array[:, 13]
     batsman_filter = batsman_array == batsman
     total_deliveries = len(ipl_matches_array[batsman_filter])
+    return total_deliveriesan_filter])
     return total_deliveries

8bf7d5c93d6b43abf9ad2b3153098aea83c0dad2
Aditya Mishra <mishraaditya6991@gmail.com>
2017-10-01 09:13:02 +0530

    Update build.py

--- Linear Regression/q02_data_splitter/build.py
+++ Linear Regression/q02_data_splitter/build.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.join(os.path.dirname(os.curdir), '..'))
 from q01_load_data.build import load_data
 
 def data_splitter(df):
-    dataframe = load_data('../../data/house_prices_multivariate.csv')
+    dataframe = load_data('data/house_prices_multivariate.csv')
     if type(dataframe) == pandas.DataFrame:
         print("True")
     else:

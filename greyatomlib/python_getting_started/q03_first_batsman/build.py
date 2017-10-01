1a97eb25cd27d572986822122b9c6061a4b23dfa
Suraj Jadhav <surajhell88@gmail.com>
2017-10-02 00:39:51 +0530

    Update build.py

--- greyatomlib/python_getting_started/q03_first_batsman/build.py
+++ greyatomlib/python_getting_started/q03_first_batsman/build.py
@@ -1,8 +1,8 @@
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(os.curdir), '..'))
+# Default Imports
+from greyatomlib.python_getting_started.q01_read_data.build import read_data
+data = read_data()
 
-
-def first_batsman(data):
-
+# Your Solution
+def first_batsman(data=data):
     name = data['innings'][0]['1st innings']['deliveries'][0][0.1]['batsman']
     return name

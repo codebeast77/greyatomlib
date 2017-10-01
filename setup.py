30280f83f7757c1511b5dbce6fbc791327211f11
Sangam Angre <sangam.angre@gmail.com>
2017-10-01 23:07:37 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,12 +17,12 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='GreyAtom-Solution',
+    name='solution',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.2',
+    version='1.0.3',
 
     description='Error handling project for greyatom students',
     long_description=long_description,

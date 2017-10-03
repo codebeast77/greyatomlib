6b8c6b0b5399f8cbb29374fa807b2f7e434894a3
gyandhanee <gyanendra.dhanee@gmail.com>
2017-10-03 12:55:32 +0530

    update day03

--- greyatomlib/numpy_advanced/q05_create_delivery_series/build.py
+++ greyatomlib/numpy_advanced/q05_create_delivery_series/build.py
@@ -1,11 +1,6 @@
 #Default Imports
-import os
-import sys
-
-sys.path.append(os.path.join(os.path.dirname(os.curdir),"..", '..'))
-
-from greyatomlib import pandas as pd
-from q01_get_total_deliveries_players.build import ipl_matches_array
+import pandas as pd
+from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
 #Your Solution
 def create_delivery_series():

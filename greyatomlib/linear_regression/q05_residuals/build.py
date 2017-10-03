b2c20c9ab61485d18921bca09aee22edcfb221b4
Jay Trivedi <mail.jaytrivedi@gmail.com>
2017-10-03 18:33:31 +0530

    Update build.py

--- greyatomlib/linear_regression/q05_residuals/build.py
+++ greyatomlib/linear_regression/q05_residuals/build.py
@@ -1,10 +1,14 @@
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(os.curdir), '..'))
-from q01_load_data.build import load_data
-from q02_data_splitter.build import data_splitter
-from q03_linear_regression.build import linear_regression
-from q04_linear_predictor.build import linear_predictor
+from greyatomlib.linear_regression.q01_load_data.build import load_data
+from greyatomlib.linear_regression.q02_data_splitter.build import data_splitter
+from greyatomlib.linear_regression.q04_linear_predictor.build import linear_predictor
+from sklearn.linear_model import LinearRegression
 
-def residuals(y, y_pred):
+dataframe = load_data('data/house_prices_multivariate.csv')
+X, y = data_splitter(dataframe)
+linear_model = LinearRegression()
+
+y_pred, _, __, ___ = linear_predictor(linear_model, X, y)
+
+def residuals(y=y, y_pred=y_pred):
     error_residuals = y - y_pred
     return error_residuals

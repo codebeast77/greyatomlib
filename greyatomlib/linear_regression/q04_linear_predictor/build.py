6e9f8aa9078d043497852acbaf8bf4b48156a870
Jay Trivedi <mail.jaytrivedi@gmail.com>
2017-10-03 18:29:45 +0530

    Update build.py

--- greyatomlib/linear_regression/q04_linear_predictor/build.py
+++ greyatomlib/linear_regression/q04_linear_predictor/build.py
@@ -1,10 +1,11 @@
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(os.curdir), '..'))
-from q01_load_data.build import load_data
-from q02_data_splitter.build import data_splitter
-from q03_linear_regression.build import linear_regression
-
+from greyatomlib.linear_regression.q01_load_data.build import load_data
+from greyatomlib.linear_regression.q02_data_splitter.build import data_splitter
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+dataframe = load_data('data/house_prices_multivariate.csv')
+X, y = data_splitter(dataframe)
+linear_model = LinearRegression()
 
 def linear_predictor(linear_model, X, y):
     y_pred = linear_model.predict(X)

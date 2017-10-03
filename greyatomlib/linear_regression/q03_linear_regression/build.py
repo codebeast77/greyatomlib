73da89a1b9801edc48f67eb371a3fd93314c3015
Jay Trivedi <mail.jaytrivedi@gmail.com>
2017-10-03 18:26:58 +0530

    Update build.py

--- greyatomlib/linear_regression/q03_linear_regression/build.py
+++ greyatomlib/linear_regression/q03_linear_regression/build.py
@@ -1,11 +1,11 @@
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(os.curdir), '..'))
-from q01_load_data.build import load_data
-from q02_data_splitter.build import data_splitter
-
+from greyatomlib.linear_regression.q01_load_data.build import load_data
+from greyatomlib.linear_regression.q02_data_splitter.build import data_splitter
 from sklearn.linear_model import LinearRegression
 
-def linear_regression(X, y):
+dataframe = load_data('data/house_prices_multivariate.csv')
+X, y = data_splitter(dataframe)
+
+def linear_regression(X=X, y=y):
     linear_regressor = LinearRegression(normalize=False)
     linear_regressor.fit(X, y)
     return linear_regressor
